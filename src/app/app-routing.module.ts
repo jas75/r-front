820bec23e97ad3a3209606f9007d17d2e3196c9c
Jason <azoulayjason@gmail.com>
2019-04-06 00:38:24 +0200

    #3 Create tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/main/main.module#MainPageModule' }
+  { path: '', loadChildren: './pages/main/main.module#MainPageModule' },
+  { path: 'community', loadChildren: './pages/community/community.module#CommunityPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'chat', loadChildren: './pages/chat/chat.module#ChatPageModule' },
+  { path: 'inbox', loadChildren: './pages/inbox/inbox.module#InboxPageModule' }
 ];
 
 @NgModule({
