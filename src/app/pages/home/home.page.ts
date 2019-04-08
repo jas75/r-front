f09ae01e442b342570f5ba0c53ea71a04679d8ba
Jason <azoulayjason@gmail.com>
2019-04-08 20:33:32 +0200

    #12 Navigation bar: done

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,43 +9,28 @@ export class HomePage implements OnInit {
 
   indexScroll = 0;
 
-  test: Array<any> = [2,3,1,5,4,9,34,0];
+  activeMenu = 1;
 
   constructor() { }
 
   ngOnInit() {
-
-    const slides = document.querySelector('ion-slides');
-      slides.options = {
+    this.slides().options = {
         effect: 'flip',
         initialSlide: 1
       };
   }
 
   navBarClick(menu: number) {
-    const slides = document.querySelector('ion-slides');
-      slides.options = {
-        effect: 'flip',
-        initialSlide: 1
-      };
-
-      slides.slideTo(menu).then(result => {
-        console.log("ca marche sur le menu" + menu);
-      })
+    this.slides().slideTo(menu).then(result => {
+        this.activeMenu = menu;
+      });
   }
 
   onSlideChange(event) {
-    // console.log(event);
+    this.slides().getActiveIndex().then(index => {
+      this.activeMenu = index;
+    });
 
-    // const slides = document.querySelector('ion-slides');
-
-    // slides.getActiveIndex().then(result => {
-    //   console.log(result);
-    // });
-
-    // slides.isBeginning().then(result => {
-    //   console.log(result);
-    // });
   }
 
   onSlidDrag(event) {
@@ -54,15 +39,21 @@ export class HomePage implements OnInit {
   }
 
 
-  onScrollUp(event) {;
-    if(event.detail.scrollTop < this.indexScroll) {
+  onScrollUp(event) {
+    if (event.detail.scrollTop < this.indexScroll) {
       const bandeau: any = document.getElementsByClassName('bandeau')[0];
       bandeau.style.marginTop = '-15px';
-    }
-    else {
+    } else {
       const bandeau: any = document.getElementsByClassName('bandeau')[0];
       bandeau.style.marginTop = '-100px';
     }
     this.indexScroll = event.detail.scrollTop;
   }
+
+  private slides() {
+    const slides = document.querySelector('ion-slides');
+    return slides;
+  }
+
+
 }
