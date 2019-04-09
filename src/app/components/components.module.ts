c5dc45f5a3b3e190bb7fd2aa16faa720a46daca1
Jason <azoulayjason@gmail.com>
2019-04-10 00:01:07 +0200

    #16 Implement cards in other pages

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { IonicModule } from '@ionic/angular';
+import { PopComponent } from './pages/pop/pop.component';
+import { NewsComponent } from './pages/news/news.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    PopComponent,
+    NewsComponent
+  ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
+    PopComponent,
+    NewsComponent
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/components/pages/pop/pop.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pop-cmp',
+  templateUrl: './pop.component.html',
+  styleUrls: ['./pop.component.scss'],
+})
+export class PopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
