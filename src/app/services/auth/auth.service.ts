a19f4e413506d3d3b3d68593ac1d6ab4e4aa329f
Jason <azoulayjason@gmail.com>
2019-04-14 02:04:02 +0200

    #20 Implement auth: fill AuthService

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,107 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { Storage } from '@ionic/storage';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Platform, AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+
+
+const TOKEN_KEY = 'access_token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  url = environment.url;
+  user = null;
+  authenticationState = new BehaviorSubject(false)
+
+  constructor(
+    private storage: Storage,
+    private helper: JwtHelperService,
+    private plt: Platform,
+    private http: HttpClient,
+    private alertController: AlertController
+  ) {
+      this.plt.ready().then(() => {
+        this.checkToken();
+      });
+  }
+
+  // check if we have a token store
+  checkToken() {
+    this.storage.get(TOKEN_KEY).then(token => {
+      if (token) {
+        let decoded = this.helper.decodeToken(token);
+        let isExpired = this.helper.isTokenExpired(token);
+
+        if (!isExpired) {
+          this.user = decoded;
+          this.authenticationState.next(true);
+        } else {
+          this.storage.remove(TOKEN_KEY);
+        }
+      }
+    });
+  }
+
+  register(credentials) {
+    return this.http.post(this.url + '/api/register', credentials).pipe(
+      catchError(e => {
+        this.showAlert(e.error.msg);
+        throw new Error(e);
+      })
+    );
+  }
+
+  login(credentials) {
+    return this.http.post(this.url + '/api/login', credentials)
+    .pipe(
+      tap(res => {
+        this.storage.set(TOKEN_KEY, res['token']);
+        this.user = this.helper.decodeToken(res['token']);
+        this.authenticationState.next(true);
+      }),
+      catchError(e => {
+        this.showAlert(e.error.msg);
+        throw new Error(e);
+      })
+    );
+  }
+
+  getSpecialData() {
+    return this.http.get(this.url + '/api/special').pipe(
+      catchError(e => {
+        let status = e.status;
+        if (status === 401) {
+          this.showAlert('You aree not authorized for this');
+          this.logout();
+        }
+        throw new Error(e);
+      })
+    )
+  }
+
+  isAuthenticated() {
+    return this.authenticationState.value;
+  }
+
+  logout() {
+    this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticationState.next(false);
+    });
+  }
+
+  showAlert(msg) {
+    let alert = this.alertController.create({
+      message: msg,
+      header: 'Error',
+      buttons: ['OK']
+    });
+
+    alert.then(thealert => thealert.present());
+  }
+
 }
