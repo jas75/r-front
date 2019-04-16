5dcc392941de9c34120924b582f09981f40e2976
Jason <azoulayjason@gmail.com>
2019-04-16 17:39:07 +0200

    #22 Create profile page: implement slides

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -7,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+
+  groupName: string = 'Home group';
+  username: string = 'username';
+  title: string = 'title de fou pour faire super long !!!!!!';
+  content: string = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, at eos? Saepe delectus, iste explicabo repellat eaque deleniti dignissimos esse rem sequi dolore commodi quod recusandae nihil doloremque. At, molestias!';
+
+
+  activeMenu = 1;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  navBarClick(menu: number) {
+    this.slides().slideTo(menu).then(result => {
+        this.activeMenu = menu;
+      });
+  }
+
+  onSlideChange(event) {
+    this.slides().getActiveIndex().then(index => {
+      this.activeMenu = index;
+    });
+
+  }
 
   onScroll(event) {
     const navbar: any = document.getElementsByClassName('navbar-menu')[0];
@@ -19,12 +40,17 @@ export class ProfilePage implements OnInit {
     if (event.detail.scrollTop  > 150) {
       console.log('tamere');
       navbar.style.position = 'fixed';
-      navbar.style.top = '50px';
+      navbar.style.top = '45px';
       slides.style.paddingTop = '60px';
     } else {
       slides.style.paddingTop = 'unset';
       navbar.style.position = 'unset';
     }
-
   }
+
+  private slides() {
+    const slides: any = document.querySelector('#slides-profile');
+    return slides;
+  }
+
 }
