fb30db8602ef5a27a1411ecbd6177873dea6d442
Jason <azoulayjason@gmail.com>
2019-04-16 02:23:38 +0200

    #22 Create profile page: design effect with the navbar

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -12,4 +12,21 @@ export class ProfilePage implements OnInit {
   ngOnInit() {
   }
 
+
+  onScroll(event) {
+    // console.log(event)
+
+    const navbar: any = document.getElementsByClassName('navbar-menu')[0];
+    const slides: any = document.getElementsByTagName('ion-slides')[0];
+    if (event.detail.scrollTop  > 150) {
+      console.log('tamere');
+      navbar.style.position = "fixed";
+      navbar.style.top = "50px";
+      slides.style.paddingTop = "55px";
+    } else {
+      slides.style.paddingTop = "unset";
+      navbar.style.position = "unset";
+    }
+
+  }
 }
