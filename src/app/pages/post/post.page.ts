361218aa4664f88f1939879b4fb73893d4193681
Jason <azoulayjason@gmail.com>
2019-05-05 13:11:49 +0200

    #32 Manage views when not logged in: Deal with post page

--- src/app/pages/post/post.page.ts
+++ src/app/pages/post/post.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { TextPage } from './text/text.page';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { LoginComponent } from 'src/app/components/UI/modals/login/login.component';
+import { SignupComponent } from 'src/app/components/UI/modals/signup/signup.component';
 
 @Component({
   selector: 'app-post',
@@ -9,11 +12,18 @@ import { TextPage } from './text/text.page';
 })
 export class PostPage implements OnInit {
 
+
+  isLoggedIn: boolean;
+
   constructor(
-    private modalController: ModalController
+    private modalController: ModalController,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
+    this.authService.authenticationState.subscribe(state => {
+      this.isLoggedIn = state;
+    });
   }
 
   dismissModal() {
@@ -28,6 +38,30 @@ export class PostPage implements OnInit {
     const modal: HTMLIonModalElement =
        await this.modalController.create({
           component: TextPage,
+          componentProps: {
+             aParameter: true,
+             otherParameter: new Date()
+          }
+    });
+    await modal.present();
+  }
+
+  async openLoginModal() {
+    const modal: HTMLIonModalElement =
+       await this.modalController.create({
+          component: LoginComponent,
+          componentProps: {
+             aParameter: true,
+             otherParameter: new Date()
+          }
+    });
+    await modal.present();
+  }
+
+  async openSignupModal() {
+    const modal: HTMLIonModalElement =
+       await this.modalController.create({
+          component: SignupComponent,
           componentProps: {
              aParameter: true,
              otherParameter: new Date()
